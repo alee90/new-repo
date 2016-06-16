0c4ea44fe3f0c8f68897739da70863409c63134b
alee90 <adlee90@gmail.com>
2016-06-16 12:22:45 -0400

    morgan/express install

---
+++ server.js
@@ -0,0 +1,15 @@
+// REQUIREMENTS
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var port = process.env.PORT || 3000;
+
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// LISTEN
+app.listen(port);
+console.log('=============================');
+console.log('Server running off PORT: ' + port);
+console.log('=============================');
