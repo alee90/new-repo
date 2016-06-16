7f1e4a9b94fd7d2dd44c0d57c2775982c41d8b08
Cat Georgoulis <catmou@gregorys-MacBook-Pro.local>
2016-06-16 12:34:31 -0400

    david motherfucker

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // REQUIREMENTS
 var express = require('express');
 var app = express();
+var router = express.Router();
 var morgan = require('morgan');
 var port = process.env.PORT || 3000;
 
 // MIDDLEWARE
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+router.get('/david', function(req, res) {
+res.send('david');
+})
+// 
 
 // LISTEN
 app.listen(port);
